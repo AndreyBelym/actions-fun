087802ccfa97a281d31455667d2a555d92c497d1
Andrey Belym <belym.a.2105@gmail.com>
2020-06-25 18:17:51 +0300

    Update test.js

--- tests/testcafe/test.js
+++ tests/testcafe/test.js
@@ -5,6 +5,6 @@ fixture `example`
 test('Example', async t => {
     console.log('test function is running...');
     console.log(require('testcafe/package').version);
-    console.log(require('testcafe-react-selectors').version);
+    console.log(require('testcafe-react-selectors'));
     await t.click('h1');
 });
