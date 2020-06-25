87bcfeafde5ec54e630ca4fb5bf2ebcf741c8d67
Andrey Belym <belym.a.2105@gmail.com>
2020-06-25 18:07:51 +0300

    Update test.js

--- tests/testcafe/test.js
+++ tests/testcafe/test.js
@@ -1,8 +1,9 @@
+
 fixture `example`
     .page `http://example.com`;
 
 test('Example', async t => {
     console.log('test function is running...');
-
+    console.log(require('testcafe/package').version);
     await t.click('h1');
 });
