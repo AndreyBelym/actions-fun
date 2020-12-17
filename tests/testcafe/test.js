ee3c0b7ddf797a94813a24e4cc9f7ea6d61aad6f
Andrey Belym <belym.a.2105@gmail.com>
2020-12-17 14:59:27 +0300

    Update test.js

--- tests/testcafe/test.js
+++ tests/testcafe/test.js
@@ -3,7 +3,8 @@ fixture `example`
     .page `http://example.com`;
 
 test('Example', async t => {
-    throw new Error();
+    if (process.versions.node.startsWith('12.'))
+        throw new Error();
     console.log('test function is running...');
     console.log(require('testcafe/package').version);
     console.log(require('testcafe-react-selectors'));
